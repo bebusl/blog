e59aef6c7fb4fb46280140a230152463e8d0506f
bebusl <bebus1998@naver.com>
2021-12-27 15:57:21 +0900

    Feat: ts useInput 훅 작성

---
+++ src/hooks/useInput.tsx
@@ -0,0 +1,29 @@
+import React, {
+  useState,
+  FormEvent,
+  ChangeEvent,
+  Dispatch,
+  SetStateAction,
+} from "react";
+
+type ReturnTypes<T> = [
+  T,
+  (e: ChangeEvent<HTMLInputElement>) => void,
+  Dispatch<SetStateAction<T>>
+];
+
+export default function useInput<T>(
+  initialValue: T,
+  validator: Function
+): ReturnTypes<T> {
+  const [value, setValue] = useState(initialValue);
+
+  const onChange = (e: ChangeEvent<HTMLInputElement>): void => {
+    e.preventDefault();
+    // if (validator(e.target.value as unknown)) {
+    setValue(e.target.value as unknown as T);
+    // }
+  };
+
+  return [value, onChange, setValue];
+}
