7564fb670591d82d0c288c26d43c167e8a438b44
bebusl <bebus1998@naver.com>
2022-02-06 21:30:22 +0900

    Style: list style modify

--- src/shared/List.tsx
+++ src/shared/List.tsx
@@ -1,6 +1,9 @@
 import styled from "styled-components";
 
 export const List = styled.div`
+  width: 100%;
+  height: 250px;
+  margin: 1rem auto;
   background-color: black;
   color: white;
   display: grid;
@@ -10,5 +13,10 @@ export const List = styled.div`
     grid-row: 1/4;
     width: 250px;
     margin: 0 auto;
+    justify-self: center;
+    align-self: center;
   }
+  & div:nth-child(2) {
+    background-color: green;
+  } //nth-child는 부모를 일단 찾고, 부모의 nth차일드를 선택하는 거임.
 `;
