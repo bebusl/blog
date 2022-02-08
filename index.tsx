5dc119f073a6b16cf7d1ba62c7980b70e1922446
bebusl <bebus1998@naver.com>
2022-02-08 18:05:45 +0900

    Chore: apollo Client server setting

--- index.tsx
+++ index.tsx
@@ -2,10 +2,29 @@ import React from "react";
 import { render } from "react-dom";
 import { BrowserRouter } from "react-router-dom";
 import App from "./src/App";
+// import { ApolloProvider } from "@apollo/react-hooks";
+// import ApolloClient from "apollo-boost";
+import {
+  ApolloClient,
+  ApolloProvider,
+  HttpLink,
+  InMemoryCache,
+} from "@apollo/client";
+import { createUploadLink } from "apollo-upload-client";
+const httpLink = createUploadLink({
+  uri: "https://weak-warthog-29.loca.lt/graphql",
+});
+
+const client = new ApolloClient({
+  link: httpLink,
+  cache: new InMemoryCache(),
+});
 
 render(
+  <ApolloProvider client={client}>
     <BrowserRouter>
-        <App />
-    </BrowserRouter>,
-    document.querySelector("#root")
+      <App />
+    </BrowserRouter>
+  </ApolloProvider>,
+  document.querySelector("#root")
 );
