46599f0d284f4fab08e836e7468bab3443743776
bebusl <bebus1998@naver.com>
2022-09-05 13:27:47 +0900

    Feat: 햄버거 메뉴바 구현

--- src/layouts/TopNav.tsx
+++ src/layouts/TopNav.tsx
@@ -35,52 +35,60 @@ const TopNavStyle = styled.ul`
   }
 `;
 
+const Container = styled.div`
+  max-width: 1200px;
+  width: 100%;
+  margin: 0 auto;
+`;
+
 const TopNav = () => {
   const isLogin = useSelector((state: RootState) => state.isLogin);
   const auth_token = useSelector((state: RootState) => state.authToken);
   const dispatch = useDispatch();
   return (
     <TopNavStyle>
-      {isLogin && (
-        <>
-          <li>
-            <Link to="/">BlogHome</Link>
-          </li>
-          <li>
-            <Link to="/writing">글쓰기</Link>
-          </li>
-          <li>
-            <Link to="/admin">관리자페이지</Link>
-          </li>
-          <li>
-            <Link
-              to=""
-              onClick={(e) => {
-                e.preventDefault();
-                dispatch(logoff());
-                setToken(auth_token, true, undefined)
-                  .get("/user/logout")
-                  .then((res) => console.log("로그아웃 결과", res))
-                  .catch((e) => console.log("로그아웃에러", e));
-              }}
-            >
-              로그아웃
-            </Link>
-          </li>
-        </>
-      )}
-      {!isLogin && (
-        <>
+      <Container>
+        {isLogin && (
           <>
             <li>
               <Link to="/">BlogHome</Link>
             </li>
             <li>
-              <Link to="/login">로그인</Link>
+              <Link to="/writing">글쓰기</Link>
+            </li>
+            <li>
+              <Link to="/admin">관리자페이지</Link>
+            </li>
+            <li>
+              <Link
+                to=""
+                onClick={(e) => {
+                  e.preventDefault();
+                  dispatch(logoff());
+                  setToken(auth_token, true, undefined)
+                    .get("/user/logout")
+                    .then((res) => console.log("로그아웃 결과", res))
+                    .catch((e) => console.log("로그아웃에러", e));
+                }}
+              >
+                로그아웃
+              </Link>
             </li>
           </>
-        </>
-      )}
+        )}
+        {!isLogin && (
+          <>
+            <>
+              <li>
+                <Link to="/">BlogHome</Link>
+              </li>
+              <li>
+                <Link to="/login">로그인</Link>
+              </li>
+            </>
+          </>
+        )}
+      </Container>
     </TopNavStyle>
   );
 };
--- src/layouts/sidenav.tsx
+++ src/layouts/sidenav.tsx
@@ -2,38 +2,83 @@ import React, { useState, Fragment } from "react";
 import styled from "styled-components";
 import { Link } from "react-router-dom";
 import { useQuery, gql } from "@apollo/client";
+import IconButton from "src/components/IconButton";
+
+const SideNav: React.FC<{
+  handleTagClick: Function;
+}> = ({ handleTagClick }) => {
+  const [nav, setNav] = useState(false);
+  const { loading, data, error } = useQuery(GET_CATEGORIES, {
+    variables: { category_: [] },
+    onCompleted: (data_) => console.log("웨이러니?", data_),
+    onError: (error_) => console.log("헤헤 에러남", error_),
+  });
+  return (
+    <>
+      <IconButton
+        onClick={() => {
+          setNav((prev) => !prev);
+        }}
+      />
+      <NavStyle nav={nav}>
+        {loading && <p>still Loading</p>}
+        {data &&
+          data.getCategoryInfo.map((adata: any) => (
+            <Fragment key={adata.category.name}>
+              <h5>
+                <Link
+                  to="/"
+                  onClick={(e) => handleTagClick(e, adata.category.id, [])}
+                >
+                  {adata.category.name}({adata.category.count})
+                </Link>
+              </h5>
+
+              <ul>
+                {adata.tags.map(
+                  (
+                    tag: { name: string; count: number; id: string },
+                    idx: number
+                  ) => (
+                    <li
+                      key={`tag${adata.category.name}-${idx}`}
+                      style={{ cursor: "pointer" }}
+                      onClick={(e) => {
+                        handleTagClick(e, null, [Number(tag.id)]);
+                      }}
+                    >
+                      {tag.name}({tag.count})
+                    </li>
+                  )
+                )}
+              </ul>
+            </Fragment>
+          ))}
+      </NavStyle>
+    </>
+  );
+};
+
+export default SideNav;
+
 const NavStyle = styled.nav<{ nav: boolean }>`
   position: fixed;
   top: 60px;
   left: 0;
-  background-color: #ededed;
+  z-index: 9999;
+  background-color: black;
   width: 250px;
+  min-height: 100vh;
   display: inline-block;
-  transform: ${(props) =>
-    props.nav ? "translateX(0px)" : "translateX(-260px)"};
+  transform: ${({ nav }) => (nav ? "translateX(0)" : "translateX(-260px)")};
   transition-duration: 1s;
-
-  & h4 {
+  margin: 0;
+  & h5 {
     font-weight: 900;
   }
   & ul {
     list-style-type: none;
   }
-`;
-
-const ToggleButton = styled.div<{ nav: boolean }>`
-  width: 60px;
-  height: 60px;
-  text-align: center;
-  line-height: 30px;
-  position: fixed;
-  left: 0;
-  top: 80px;
-  transform: ${(props) =>
-    props.nav ? "translateX(150px)" : "translateX(0px)"};
-  transition-duration: 0.7s;
-  z-index: 1;
-  cursor: pointer;
 `;
 
 const category_info = gql`
@@ -66,68 +111,3 @@ const GET_CATEGORIES = gql`
     }
   }
 `;
-
-const SideNav: React.FC<{
-  handleTagClick: Function;
-}> = ({ handleTagClick }) => {
-  const [nav, setNav] = useState(false);
-  const { loading, data, error } = useQuery(GET_CATEGORIES, {
-    variables: { category_: [] },
-    // fetchPolicy: "no-cache",
-    onCompleted: (data_) => console.log("웨이러니?", data_),
-    onError: (error_) => console.log("헤헤 에러남", error_),
-  });
-  return (
-    <>
-      <ToggleButton
-        onClick={(e) => {
-          e.preventDefault();
-          setNav(!nav);
-        }}
-        nav={nav}
-      >
-        <img
-          alt="버거바"
-          width="10px"
-          src="https://cdn-icons.flaticon.com/png/512/4204/premium/4204600.png?token=exp=1644595427~hmac=db200c0d1cea00509e4bd75e5cdabd46"
-        />
-      </ToggleButton>
-      <NavStyle nav={nav}>
-        {loading && <p>still Loading</p>}
-        {data &&
-          data.getCategoryInfo.map((adata: any) => (
-            <Fragment key={adata.category.name}>
-              <h4>
-                <Link
-                  to="/"
-                  onClick={(e) => handleTagClick(e, adata.category.id, [])}
-                >
-                  {adata.category.name}({adata.category.count})
-                </Link>
-              </h4>
-              <ul>
-                {adata.tags.map(
-                  (
-                    tag: { name: string; count: number; id: string },
-                    idx: number
-                  ) => (
-                    <li
-                      key={`tag${adata.category.name}-${idx}`}
-                      style={{ cursor: "pointer" }}
-                      onClick={(e) => {
-                        handleTagClick(e, null, [Number(tag.id)]);
-                      }}
-                    >
-                      {tag.name}({tag.count})
-                    </li>
-                  )
-                )}
-              </ul>
-            </Fragment>
-          ))}
-      </NavStyle>
-    </>
-  );
-};
-
-export default SideNav;
