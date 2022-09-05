d80a45d7834a75c12c9d8738585e32c184fff286
bebusl <bebus1998@naver.com>
2022-09-05 19:16:59 +0900

    Chore: redux legacy 코드 삭제

--- src/hooks/useInput.ts
+++ src/hooks/useInput.ts
@@ -12,10 +12,7 @@ type ReturnTypes<T> = [
   Dispatch<SetStateAction<T>>
 ];
 
-export default function useInput<T>(
-  initialValue: T,
-  validator: Function
-): ReturnTypes<T> {
+export default function useInput<T>(initialValue: T): ReturnTypes<T> {
   const [value, setValue] = useState(initialValue);
 
   const onChange = (e: ChangeEvent<HTMLInputElement>): void => {
