b913f2ead4d5f95931eefb69f3857ac2e4ffe2c2
bebusl <bebus1998@naver.com>
2022-09-05 13:39:03 +0900

    Feat: 일정 폭을 가지는 기본 컨테이너 구현

---
+++ src/layouts/DefaultContainer.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import styled from "styled-components";
+import { FlexBox } from "src/components/FlexBox";
+
+const DefaultContainer = styled(FlexBox)`
+  max-width: 1200px;
+  width: 100%;
+  height: 100%;
+  margin: 0 auto;
+`;
+
+export default DefaultContainer;
