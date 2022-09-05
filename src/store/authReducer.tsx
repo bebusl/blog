264ce48cd40f285bc5aa8f2efcdd97fa2bead1d2
bebusl <bebus1998@naver.com>
2022-09-05 18:24:39 +0900

    Feat: redux 로그인 thunks 및 reducer 작성

---
+++ src/store/authReducer.tsx
@@ -0,0 +1,58 @@
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+import axios, { AxiosError } from "axios";
+import setCookie from "src/utils/setCookie";
+import { ids } from "webpack";
+
+export const login = createAsyncThunk(
+  "auth/login",
+  async (userData: { email: string; password: string }, thunkAPI) => {
+    try {
+      const response = await axios.post(
+        "https://jh-blog-api.yoonleeverse.com/user/login",
+        userData,
+        {
+          withCredentials: true,
+        }
+      );
+      console.log(response);
+      return response.data;
+    } catch (error) {
+      const errorWrap = error as AxiosError;
+      return thunkAPI.rejectWithValue(errorWrap.response?.status);
+    }
+  }
+);
+
+const initialState = {
+  isLoading: false,
+  isLogin: false,
+  email: "",
+  authToken: "",
+};
+
+export const authSlice = createSlice({
+  name: "auth",
+  initialState: initialState,
+  reducers: {},
+  extraReducers: (builder) => {
+    builder.addCase(login.fulfilled, (state, action) => {
+      setCookie("refreshToken", action.payload.refresh_token, 7);
+      state.isLoading = false;
+      state.isLogin = true;
+      state.email = action.payload.user.email;
+      state.authToken = action.payload.auth_token;
+    });
+    builder.addCase(login.rejected, (state, action) => {
+      state.isLoading = false;
+      state.isLogin = false;
+      state.authToken = "";
+      state.email = "";
+    });
+    builder.addCase(login.pending, (state, action) => {
+      state.isLoading = true;
+    });
+  },
+});
+
+export const {} = authSlice.actions;
+export default authSlice.reducer;
